d0e4f57956616d7a78a4200f8dbf73e2a7eb7931
Patrick Lindenberg <heapdump@icloud.com>
2021-08-27 20:23:10 +0200

    add additional colors, extend demo text

--- src/main/java/org/antlr/intellij/plugin/configdialogs/ANTLRv4ColorsPage.java
+++ src/main/java/org/antlr/intellij/plugin/configdialogs/ANTLRv4ColorsPage.java
@@ -14,12 +14,17 @@ import javax.swing.*;
 import java.util.Map;
 
 public class ANTLRv4ColorsPage implements ColorSettingsPage {
-    private static final AttributesDescriptor[] ATTRIBUTES =
-            {
-                    new AttributesDescriptor("Lexer Rule", ANTLRv4SyntaxHighlighter.TOKENNAME),
-                    new AttributesDescriptor("Parser Rule", ANTLRv4SyntaxHighlighter.RULENAME),
-            };
+    private static final AttributesDescriptor[] ATTRIBUTES = {
+            new AttributesDescriptor("Lexer rule", ANTLRv4SyntaxHighlighter.TOKENNAME),
+            new AttributesDescriptor("Parser rule", ANTLRv4SyntaxHighlighter.RULENAME),
+            new AttributesDescriptor("Keyword", ANTLRv4SyntaxHighlighter.KEYWORD),
+            new AttributesDescriptor("String", ANTLRv4SyntaxHighlighter.STRING),
+            new AttributesDescriptor("Number", ANTLRv4SyntaxHighlighter.INT),
+            new AttributesDescriptor("Block comment", ANTLRv4SyntaxHighlighter.BLOCK_COMMENT),
+            new AttributesDescriptor("Line comment", ANTLRv4SyntaxHighlighter.LINE_COMMENT),
+            new AttributesDescriptor("Doc comment", ANTLRv4SyntaxHighlighter.DOC_COMMENT),
 
+    };
 
     @Nullable
     @Override
@@ -39,7 +44,7 @@ public class ANTLRv4ColorsPage implements ColorSettingsPage {
     @Override
     public String getDemoText() {
         return
-                "grammar Foo;\n" +
+                "grammar Foo;\n\n" + "options { tokenVocab=DSPLLexer; }\n\n" +
                         "\n" +
                         "compilationUnit : STUFF EOF;\n" +
                         "\n" +
@@ -47,6 +52,7 @@ public class ANTLRv4ColorsPage implements ColorSettingsPage {
                         "WS : [ \\t]+ -> channel(HIDDEN);\n" +
                         "NEWLINE : [\\r\\n]+ -> type(WS);\n" +
                         "BAD_CHAR : . -> skip;\n" +
+                        "INT : [1..9] [0..9]*\n" +
                         "\n" +
                         "mode OTHER_MODE;\n" +
                         "\n" +
