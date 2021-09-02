ad27a8e720e34468203b2ef328175a705f496a29
Patrick Lindenberg <heapdump@icloud.com>
2021-09-02 18:53:13 +0200

    The following major changes where made: + reformat code + add basic java doc + add different colors for different types of nodes + add auto-scale + add node selection via mouse click + add min. width of tree nodes and fix text layout
    Debug code will be removed later.

--- src/main/java/org/antlr/intellij/plugin/preview/UberTreeViewer.java
+++ src/main/java/org/antlr/intellij/plugin/preview/UberTreeViewer.java
@@ -1,7 +1,5 @@
 package org.antlr.intellij.plugin.preview;
 
-import com.intellij.ui.DarculaColors;
-import com.intellij.ui.Gray;
 import com.intellij.ui.JBColor;
 import com.intellij.util.ui.JBFont;
 import org.abego.treelayout.NodeExtentProvider;
@@ -17,40 +15,53 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 import org.antlr.v4.runtime.tree.Tree;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
+import java.awt.event.MouseEvent;
 import java.awt.geom.CubicCurve2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.awt.RenderingHints.*;
 
 
 /**
- *
+ * Custom tree layout viewer component.
+ * Enhanced version based on: {@code TreeViewer}
  */
 public class UberTreeViewer extends TreeViewer {
 
+    private final List<ParsingResultSelectionListener> selectionListeners = new ArrayList<>();
+
     private final boolean highlightUnreachedNodes;
-    private final boolean useCurvedEdges;
 
     protected JBColor unreachableColor;
     protected JBColor edgesColor;
     protected JBColor errorColor;
     protected JBColor endOfFileColor;
     protected JBColor terminalNodeColor;
+    protected JBColor selectedNodeColor;
 
     protected int minCellWidth;
-    private float edgesStrokeWidth;
+    protected float edgesStrokeWidth;
+
+    protected boolean autoscaling;
+
+    protected Point2D offset;
+
+    private long count;
+
+    protected Tree selectedTreeNode;
 
 
     /**
-     *
+     * Provides layout information based on font and text of a tree node.
      */
-    public static class VariableExtentProvide implements NodeExtentProvider<Tree> {
+    public static class VariableExtentProvider implements NodeExtentProvider<Tree> {
         UberTreeViewer viewer;
 
 
-        public VariableExtentProvide(UberTreeViewer viewer) {
+        public VariableExtentProvider(UberTreeViewer viewer) {
             this.viewer = viewer;
         }
 
@@ -60,7 +71,12 @@ public class UberTreeViewer extends TreeViewer {
             FontMetrics fontMetrics = viewer.getFontMetrics(viewer.font);
             String s = viewer.getText(tree);
             int w = fontMetrics.stringWidth(s) + viewer.nodeWidthPadding * 2;
-            //   System.out.println("getWidth(" + s + ") = " + w);
+
+            // Do not use min size for terminals.
+            if (tree instanceof TerminalNode) {
+                return w;
+            }
+
             return Math.max(w, viewer.minCellWidth);
         }
 
@@ -72,25 +88,40 @@ public class UberTreeViewer extends TreeViewer {
             String s = viewer.getText(tree);
             String[] lines = s.split("\n");
             //   System.out.println("getHeight(" + s + ") = " + h * lines.length + " lines=" + lines.length);
-
-            return h * lines.length;
+            return h + (lines.length - 1) * fontMetrics.getHeight();
         }
     }
 
 
+    /**
+     * Creates the UberTreeViewer component based on the given tree node.
+     *
+     * @param ruleNames               A list of rule names.
+     * @param tree                    The root tree node of the diagram.
+     * @param highlightUnreachedNodes If set unreached nodes are highlighted.
+     */
     public UberTreeViewer(List<String> ruleNames, Tree tree, boolean highlightUnreachedNodes) {
         super(ruleNames, tree);
         this.highlightUnreachedNodes = highlightUnreachedNodes;
 
+        this.highlightedNodes = new ArrayList<>();
+
+        /* draw offset for diagram - needed to draw centered */
+        offset = new Point2D.Double(0, 0);
+        autoscaling = true;
+
+        /* font setup */
         fontSize = 13;
         font = JBFont.regular().deriveFont((float) (fontSize));
         fontName = font.getFontName();
 
-        useCurvedEdges = true;
+        /* edges setup */
+        boolean useCurvedEdges = true;
         setUseCurvedEdges(useCurvedEdges);
         edgesColor = JBColor.BLACK;
-        edgesStrokeWidth = 1.4f;
+        edgesStrokeWidth = 1.3f;
 
+        /* color and shape setup */
         boxColor = JBColor.BLUE;
         terminalNodeColor = JBColor.ORANGE;
         endOfFileColor = JBColor.DARK_GRAY;
@@ -98,14 +129,24 @@ public class UberTreeViewer extends TreeViewer {
         arcSize = 9;
         minCellWidth = 100;
         gapBetweenLevels = 30;
-        nodeHeightPadding = 12;
-        nodeWidthPadding = 7;
+        nodeHeightPadding = 6;
+        nodeWidthPadding = 6;
 
+        highlightedBoxColor = JBColor.PINK;
+        selectedNodeColor = JBColor.PINK;
         unreachableColor = JBColor.orange;
         errorColor = JBColor.RED;
         textColor = JBColor.WHITE;
 
-        setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        // setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    }
+
+
+    /**
+     * Registers a new rule selection listener.
+     */
+    public void addParsingResultSelectionListener(ParsingResultSelectionListener listener) {
+        selectionListeners.add(listener);
     }
 
 
@@ -117,7 +158,7 @@ public class UberTreeViewer extends TreeViewer {
 
             Rectangle2D.Double parentBounds = getBoundsOfNode(parent);
             double x1 = parentBounds.getCenterX();
-            double y1 = parentBounds.getMaxY() - 3;
+            double y1 = parentBounds.getMaxY();
 
             g.setColor(edgesColor);
 
@@ -127,11 +168,12 @@ public class UberTreeViewer extends TreeViewer {
                 double y2 = childBounds.getMinY();
 
                 if (getUseCurvedEdges() && x1 != x2) {
+                    double alpha = Math.abs(x1 - x2) * 0.1;
                     CubicCurve2D c = new CubicCurve2D.Double();
                     double ctrlx1 = x1;
-                    double ctrly1 = y1 + 15;
+                    double ctrly1 = y1 + alpha;
                     double ctrlx2 = x2;
-                    double ctrly2 = y2 - 15;
+                    double ctrly2 = y2 - alpha;
                     c.setCurve(x1, y1, ctrlx1, ctrly1, ctrlx2, ctrly2, x2, y2);
                     ((Graphics2D) g).draw(c);
                 } else {
@@ -144,11 +186,54 @@ public class UberTreeViewer extends TreeViewer {
     }
 
 
+    /**
+     * Detects the correct scale-factor and offset to draw the diagram to fit in the window.
+     */
+    protected void autoScaleTree() {
+        double factor, offs;
+
+        double xRatio = (double) (getParent().getWidth() - 25) / (treeLayout.getBounds().getWidth() + offset.getX());
+        double yRatio = (double) (getParent().getHeight() - 25) / (treeLayout.getBounds().getHeight() + offset.getY());
+
+        factor = Math.min(xRatio, yRatio);
+        scale = factor;// Math.min(factor, 1.5);
+
+        offs = (double) getParent().getWidth() / 2 - treeLayout.getBounds().getWidth() * scale / 2;
+        offs = offs * (1. / scale);
+        offset.setLocation(offs, 0);
+    }
+
+
+    /**
+     * Paint component.
+     *
+     * @param g Graphics context.
+     * @see javax.swing.JComponent
+     */
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
+        long time = System.nanoTime();
 
-        if (treeLayout == null) return;
+        ArrayList<String> buff = new ArrayList<>();
+
+        buff.add("PARAMETER:\n");
+        buff.add("parent: " + getParent().getWidth() + "x" + getParent().getHeight());
+
+
+        if (treeLayout != null) {
+            if (autoscaling) autoScaleTree();
+            super.paint(g);
+        } else {
+            super.paint(g);
+            return;
+        }
+
+        buff.add("fc&off: " + String.format("%.3f", scale) + " / " + String.format("%.3f", offset.getX()));
+        buff.add("compon: " + getWidth() + "x" + getHeight());
+        buff.add("tree  : " + treeLayout.getBounds().getWidth() + "x" + treeLayout.getBounds().getHeight());
+        buff.add("tree N: " + String.format("%.3f", treeLayout.getBounds().getWidth() * scale) + "x" + String.format("%.3f", treeLayout.getBounds().getHeight() * scale));
+        buff.add("\n");
+
 
         Graphics2D g2 = (Graphics2D) g;
 
@@ -165,13 +250,49 @@ public class UberTreeViewer extends TreeViewer {
 
         paintEdges(g, getTree().getRoot());
 
+//        Rectangle2D bnd = treeLayout.getBounds();
+//
+//
+//        g2.setColor(JBColor.RED.darker().darker().darker());
+//        g2.fillRect((int) (bnd.getX() + offset.getX()), (int) (bnd.getY() + offset.getY()), (int) bnd.getWidth(), (int) bnd.getHeight());
+
+
         // paint the boxes
         for (Tree Tree : treeLayout.getNodeBounds().keySet()) {
             paintBox(g, Tree);
         }
+
+        double delta = ((double) System.nanoTime() - time) / 1000000.;
+
+
+        Font saved = g2.getFont();
+
+        Font menlo = new Font("Menlo", Font.PLAIN, 14);
+
+        g2.setFont(menlo.deriveFont((float) (13. * (1. / scale))).deriveFont(Font.BOLD));
+        g2.setColor(JBColor.BLACK);
+
+        buff.add("count: #" + count++);
+        buff.add("time: " + String.format("%.3f", delta) + "ms");
+
+        int y = 10;
+        for (String s : buff) {
+            g2.drawString(s, 10, y);
+            y += 18 * (1. / scale);
+        }
+
+        g2.setFont(saved);
+
     }
 
 
+    /**
+     * Draws a custom diagram box based on the tree nodes
+     * layout information,
+     *
+     * @param g    Graphics context.
+     * @param tree The tree node.
+     */
     @Override
     protected void paintBox(Graphics g, Tree tree) {
         customPaintBox(g, tree);
@@ -196,6 +317,12 @@ public class UberTreeViewer extends TreeViewer {
     }
 
 
+    /**
+     * Custom diagram box paint method.
+     *
+     * @param g    Graphics Context.
+     * @param tree Tree-Node to paint.
+     */
     private void customPaintBox(Graphics g, Tree tree) {
         Rectangle2D.Double box = getBoundsOfNode(tree);
 
@@ -210,20 +337,39 @@ public class UberTreeViewer extends TreeViewer {
         Color color;
         int boxRoundness = 1;
 
-        if (isHighlighted(tree)) color = highlightedBoxColor;
-        else if (tree instanceof ErrorNode || ruleFailedAndMatchedNothing) color = errorColor;
+
+        if (tree instanceof ErrorNode || ruleFailedAndMatchedNothing) color = errorColor;
         else if (tree instanceof TerminalNode) {
             Token token = ((TerminalNode) tree).getSymbol();
             if (token.getText().equals("<EOF>")) {
                 color = endOfFileColor;
-                boxRoundness = 3;
+                boxRoundness = 2;
             } else {
                 color = terminalNodeColor;
-                boxRoundness = 6;
+                boxRoundness = 0;
             }
 
         } else if (boxColor != null) color = boxColor;
         else color = null;
+
+
+        /* selected node handled here */
+        if (isSelectedTreeNode(tree)) {
+            color = selectedNodeColor;
+
+            BasicStroke stroke = new BasicStroke(0.8f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+            ((Graphics2D) g).setStroke(stroke);
+
+            g.setColor(selectedNodeColor.brighter());
+            g.drawRoundRect(
+                    (int) Math.round(box.x - 3),
+                    (int) Math.round(box.y - 3),
+                    (int) Math.round(box.width + 6),
+                    (int) Math.round(box.height + 6),
+                    arcSize * boxRoundness,
+                    arcSize * boxRoundness
+            );
+        }
 
         /* fill box */
         if (color != null) {
@@ -253,27 +399,75 @@ public class UberTreeViewer extends TreeViewer {
 
         // ---------------- PAINT LABELS AND TEXT -------------------------------
 
-        if (tree instanceof ErrorNode || ruleFailedAndMatchedNothing) {
-            g.setColor(errorColor);
-        } else {
-            g.setColor(textColor);
+//        if (tree instanceof ErrorNode || ruleFailedAndMatchedNothing) {
+//            g.setColor(errorColor);
+//        } else {
+//            g.setColor(textColor);
+//        }
+
+        g.setFont(font);
+        g.setColor(textColor);
+
+        if (tree.getParent() == null) {
+            g.setColor(JBColor.WHITE);
+            g.setFont(font.deriveFont(Font.BOLD).deriveFont((float) fontSize));
         }
+
+//        if (tree instanceof TerminalNode) {
+//            g.setFont(font.deriveFont(Font.ITALIC).deriveFont((float) fontSize ));
+//        }
+        //s = Utils.escapeWhitespace(s, true);
+
 
         String s = getText(tree);
-        String[] lines = s.split("\n");
-        FontMetrics m = getFontMetrics(font);
-        int y = (int) Math.round(box.y + m.getAscent() + m.getLeading() + nodeHeightPadding);
+        FontMetrics m = getFontMetrics(getFont());
+        int y = (int) Math.round(box.y + box.height / 2. - m.getHeight() / 2. + m.getAscent());
+        int x = (int) Math.round(box.x + box.width / 2. - m.stringWidth(s) / 2.);
 
-        for (String line : lines) {
-            int strWidth = m.stringWidth(line);
-            int x = (int) box.x + (int) Math.round(box.width / 2. - strWidth / 2.);
-
-            text(g, line, x, y);
-            y += m.getHeight();
-        }
+        text(g, s, x, y);
     }
 
 
+    /**
+     * Returns the bounds of a tree node including the offset vector.
+     *
+     * @param node The tree node.
+     * @return Bounds as {@code Rectangle2D}.
+     */
+    @Override
+    protected Rectangle2D.Double getBoundsOfNode(Tree node) {
+        Rectangle2D.Double bounds = treeLayout.getNodeBounds().get(node);
+
+        Rectangle2D.Double newBounds = new Rectangle2D.Double(
+                bounds.x + offset.getX(),
+                bounds.y + offset.getY(),
+                bounds.width, bounds.height
+        );
+
+        return newBounds;
+    }
+
+
+    /**
+     * Get tree node label from: {@code TreeTextProvider}.
+     *
+     * @param tree The tree node.
+     * @return The labels as string.
+     * @see AltLabelTextProvider
+     */
+    @Override
+    protected String getText(Tree tree) {
+        String s = treeTextProvider.getText(tree);
+        s = Utils.escapeWhitespace(s, false);
+        return s;
+    }
+
+
+    /**
+     * Returns the dimension of the tree layout
+     *
+     * @return
+     */
     private Dimension getScaledTreeSize() {
         Dimension scaledTreeSize = treeLayout.getBounds().getBounds().getSize();
         scaledTreeSize = new Dimension(
@@ -286,28 +480,47 @@ public class UberTreeViewer extends TreeViewer {
     }
 
 
+    /**
+     * Update the component's size based on the tree layout size.
+     */
     private void updatePreferredSize() {
         setPreferredSize(getScaledTreeSize());
         invalidate();
-        if (getParent() != null) {getParent().validate();}
+
+        if (getParent() != null)
+            getParent().validate();
+
         repaint();
     }
 
 
+    /**
+     * Helper method to draw text on a Graphics context.
+     * Whitespaces are escaped except spaces.
+     *
+     * @param g Graphics context
+     * @param s The string to be drawn.
+     * @param x The X coordinate to draw at.
+     * @param y The Y coordinate to draw at.
+     */
     @Override
     public void text(Graphics g, String s, int x, int y) {
-        //    System.out.println("drawing '" + s + "' @ " + x + "," + y);
         s = Utils.escapeWhitespace(s, false);
         g.drawString(s, x, y);
     }
 
 
+    /**
+     * Creates a tree layout by out of a tree node.
+     *
+     * @param root The root node of the tree.
+     */
     @Override
     public void setTree(Tree root) {
         if (root != null) {
             treeLayout = new TreeLayout<>(
                     getTreeLayoutAdaptor(root),
-                    new VariableExtentProvide(this),
+                    new VariableExtentProvider(this),
                     new DefaultConfiguration<>(gapBetweenLevels, gapBetweenNodes),
                     true
             );
@@ -321,7 +534,85 @@ public class UberTreeViewer extends TreeViewer {
     }
 
 
+    /**
+     * Test if a valid tree layout has been assigned.
+     *
+     * @return True if assigned.
+     */
     public boolean hasTree() {
         return treeLayout != null;
     }
+
+
+    /**
+     * Handle mouse events and may raise an event at the {@code ParsingResultSelectionListener}
+     * to notify all subscriber.
+     *
+     * @param e Mouse Event
+     * @see ParsingResultSelectionListener
+     */
+    protected void handleMouseEvent(MouseEvent e) {
+        // do nothing on an invalid tree
+        if (treeLayout == null || treeLayout.getLevelCount() == 0) return;
+
+        // always clear on click
+        setSelectedTreeNode(null);
+
+        for (Tree tree : treeLayout.getNodeBounds().keySet()) {
+            Rectangle2D.Double box = getBoundsOfNode(tree);
+
+            if (box.contains(e.getX() / scale, e.getY() / scale)) {
+                // set selected node
+                setSelectedTreeNode(tree);
+
+                // raise event for all selection listeners
+                for (ParsingResultSelectionListener listener : selectionListeners) {
+                    listener.onParserRuleSelected(tree);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Checks for selected tree node.
+     *
+     * @param tree The tree node.
+     * @return True if node matches.
+     */
+    public boolean isSelectedTreeNode(Tree tree) {
+        if (selectedTreeNode == null) return false;
+        return tree == selectedTreeNode;
+    }
+
+
+    /**
+     * Get selected tree node.
+     *
+     * @return Tree node instance. {@code Tree}
+     */
+    public Tree getSelectedTreeNode() {
+        return selectedTreeNode;
+    }
+
+
+    /**
+     * Set the current selected tree node.
+     *
+     * @param tree The tree node.
+     */
+    public void setSelectedTreeNode(Tree tree) {
+        this.selectedTreeNode = tree;
+    }
+
+
+    /**
+     * Returns the assigned tree layout.
+     *
+     * @return The tree layout: {@code TreeLayout}.
+     */
+    public TreeLayout<Tree> getTreeLayout() {
+        return treeLayout;
+    }
+
 }
