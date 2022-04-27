52f8d35eee581d9037acbd48e49d2dd746b41e15
Patrick Lindenberg <heapdump@icloud.com>
2022-04-27 11:28:44 +0200

    fix style inheritance

--- src/main/java/org/antlr/intellij/plugin/preview/ui/treenodes/SelectedTreeNode.java
+++ src/main/java/org/antlr/intellij/plugin/preview/ui/treenodes/SelectedTreeNode.java
@@ -1,18 +1,24 @@
 package org.antlr.intellij.plugin.preview.ui.treenodes;
 
+import com.intellij.ui.JBColor;
 import org.antlr.intellij.plugin.preview.ui.DefaultStyles;
+import org.antlr.intellij.plugin.preview.ui.StyleProperties;
 import org.antlr.intellij.plugin.preview.ui.StyledElement;
 
 import java.awt.geom.Rectangle2D;
 
+/**
+ * Selected tree-node.
+ */
 public class SelectedTreeNode extends BasicStyledTreeNode {
 
-    public SelectedTreeNode() {
-        styleProperties = DefaultStyles.ERROR_NODE_STYLE;
-    }
 
-
-    public SelectedTreeNode(StyledElement parent, Rectangle2D viewport) {
+    public SelectedTreeNode(StyledElement parent, Rectangle2D viewport, StyleProperties base) {
         super(parent, viewport, DefaultStyles.SELECTED_NODE_STYLE);
+        this.setStyleProperties(base);
+
+        styleProperties.setStroke(DefaultStyles.THICK_STROKE);
+        setOutlineColor((JBColor) (shape.hasOutlineColor() ? getOutlineColor() : getForeground()).brighter());
+        setBackground((JBColor) getBackground().brighter());
     }
 }
