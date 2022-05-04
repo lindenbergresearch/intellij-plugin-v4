0632f5022e9413efc2408970e19e0b77e79600b4
Patrick Lindenberg <heapdump@icloud.com>
2022-05-04 11:38:05 +0200

    change outline color

--- src/main/java/org/antlr/intellij/plugin/preview/ui/treenodes/RootTreeNode.java
+++ src/main/java/org/antlr/intellij/plugin/preview/ui/treenodes/RootTreeNode.java
@@ -1,19 +1,21 @@
 package org.antlr.intellij.plugin.preview.ui.treenodes;
 
+import com.intellij.ui.JBColor;
 import org.antlr.intellij.plugin.preview.ui.DefaultStyles;
 import org.antlr.intellij.plugin.preview.ui.StyledElement;
 
 import java.awt.geom.Rectangle2D;
 
 public class RootTreeNode extends BasicStyledTreeNode {
-
-
+    
+    
     public RootTreeNode() {
         styleProperties = DefaultStyles.ROOT_NODE_STYLE;
     }
-
-
+    
+    
     public RootTreeNode(StyledElement parent, Rectangle2D viewport) {
         super(parent, viewport, DefaultStyles.ROOT_NODE_STYLE);
+        setOutlineColor((JBColor) getBackground().darker());
     }
 }
