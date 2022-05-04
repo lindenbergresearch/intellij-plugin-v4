241d46b446cfb2b100ae8f75db4cf8fa60fc1e63
Patrick Lindenberg <heapdump@icloud.com>
2022-05-04 11:36:24 +0200

    add small terminal font for footer label

--- src/main/java/org/antlr/intellij/plugin/preview/ui/treenodes/TerminalTreeNode.java
+++ src/main/java/org/antlr/intellij/plugin/preview/ui/treenodes/TerminalTreeNode.java
@@ -2,41 +2,35 @@ package org.antlr.intellij.plugin.preview.ui.treenodes;
 
 import org.antlr.intellij.plugin.preview.ui.DefaultStyles;
 import org.antlr.intellij.plugin.preview.ui.StyledElement;
-import org.antlr.intellij.plugin.preview.ui.StyledRoundRect;
 
-import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
 public class TerminalTreeNode extends BasicStyledTreeNode {
-    public static final float OFFSET = -10f;
-
+    public static final float OFFSET = 0f;
+    
     // use small labels
     boolean smallLabel;
-
-
+    
+    
     public TerminalTreeNode() {
         styleProperties = DefaultStyles.ERROR_NODE_STYLE;
     }
-
-
+    
+    
     public TerminalTreeNode(StyledElement parent, Rectangle2D viewport) {
         super(parent, viewport, DefaultStyles.TERMINAL_NODE_STYLE);
         shiftViewport(0, OFFSET);
-
-        if (shape instanceof StyledRoundRect) {
-            ((StyledRoundRect) shape).setArc(new Dimension(20, 20));
-        }
     }
-
-
+    
+    
     public boolean isSmallLabel() {
         return smallLabel;
     }
-
-
+    
+    
     public void setSmallLabel(boolean smallLabel) {
         this.smallLabel = smallLabel;
-
+        
         if (smallLabel)
             setFont(DefaultStyles.SMALL_TERMINAL_FONT);
         else
