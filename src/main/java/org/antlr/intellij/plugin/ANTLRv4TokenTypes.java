3bb54b8dbb164f3fed9abf4cf39c5c2f614588b2
Patrick Lindenberg <heapdump@icloud.com>
2022-05-05 11:37:20 +0200

    add operators

--- src/main/java/org/antlr/intellij/plugin/ANTLRv4TokenTypes.java
+++ src/main/java/org/antlr/intellij/plugin/ANTLRv4TokenTypes.java
@@ -15,10 +15,10 @@ import static org.antlr.intellij.adaptor.lexer.PSIElementTypeFactory.*;
 public class ANTLRv4TokenTypes {
     public static final List<TokenIElementType> TOKEN_ELEMENT_TYPES =
         getTokenIElementTypes(ANTLRv4Language.INSTANCE);
-
+    
     public static final List<RuleIElementType> RULE_ELEMENT_TYPES =
         getRuleIElementTypes(ANTLRv4Language.INSTANCE);
-
+    
     public static final TokenSet COMMENTS =
         createTokenSet(
             ANTLRv4Language.INSTANCE,
@@ -26,13 +26,26 @@ public class ANTLRv4TokenTypes {
             ANTLRv4Lexer.BLOCK_COMMENT,
             ANTLRv4Lexer.LINE_COMMENT
         );
-
+    
     public static final TokenSet WHITESPACES =
         createTokenSet(
             ANTLRv4Language.INSTANCE,
             ANTLRv4Lexer.WS
         );
-
+    
+    public static final TokenSet OPERATOR =
+        createTokenSet(
+            ANTLRv4Language.INSTANCE,
+            ANTLRv4Lexer.PLUS,
+            ANTLRv4Lexer.PLUS_ASSIGN,
+            ANTLRv4Lexer.STAR,
+            ANTLRv4Lexer.GT,
+            ANTLRv4Lexer.LT,
+            ANTLRv4Lexer.ASSIGN,
+            ANTLRv4Lexer.OR
+        );
+    
+    
     public static final TokenSet BRACES =
         createTokenSet(
             ANTLRv4Language.INSTANCE,
@@ -43,25 +56,34 @@ public class ANTLRv4TokenTypes {
             ANTLRv4Lexer.OPT_LBRACE,
             ANTLRv4Lexer.OPT_RBRACE
         );
-
-
+    
+    
     public static final TokenSet KEYWORDS =
         createTokenSet(
             ANTLRv4Language.INSTANCE,
-            ANTLRv4Lexer.LEXER, ANTLRv4Lexer.PROTECTED, ANTLRv4Lexer.IMPORT, ANTLRv4Lexer.CATCH,
-            ANTLRv4Lexer.PRIVATE, ANTLRv4Lexer.FRAGMENT, ANTLRv4Lexer.PUBLIC, ANTLRv4Lexer.MODE,
-            ANTLRv4Lexer.FINALLY, ANTLRv4Lexer.RETURNS, ANTLRv4Lexer.THROWS, ANTLRv4Lexer.GRAMMAR,
-            ANTLRv4Lexer.LOCALS, ANTLRv4Lexer.PARSER
+            ANTLRv4Lexer.PROTECTED, ANTLRv4Lexer.IMPORT, ANTLRv4Lexer.CATCH,
+            ANTLRv4Lexer.PRIVATE, ANTLRv4Lexer.PUBLIC, ANTLRv4Lexer.FINALLY,
+            ANTLRv4Lexer.RETURNS, ANTLRv4Lexer.THROWS, ANTLRv4Lexer.LOCALS
         );
-
+    
+    public static final TokenSet SPECIAL_KEYWORD =
+        createTokenSet(
+            ANTLRv4Language.INSTANCE,
+            ANTLRv4Lexer.LEXER,
+            ANTLRv4Lexer.FRAGMENT,
+            ANTLRv4Lexer.MODE,
+            ANTLRv4Lexer.GRAMMAR,
+            ANTLRv4Lexer.PARSER
+        );
+    
     public static IElementType BAD_TOKEN_TYPE = new IElementType("BAD_TOKEN", ANTLRv4Language.INSTANCE);
-
-
+    
+    
     public static RuleIElementType getRuleElementType(@MagicConstant(valuesFromClass = ANTLRv4Parser.class) int ruleIndex) {
         return RULE_ELEMENT_TYPES.get(ruleIndex);
     }
-
-
+    
+    
     public static TokenIElementType getTokenElementType(@MagicConstant(valuesFromClass = ANTLRv4Lexer.class) int ruleIndex) {
         return TOKEN_ELEMENT_TYPES.get(ruleIndex);
     }
