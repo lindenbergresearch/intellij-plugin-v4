6e271d0bb926fd0f684688e4677f7004107e0a54
Patrick Lindenberg <heapdump@icloud.com>
2022-05-05 19:00:19 +0200

    add a second keyword class

--- src/main/java/org/antlr/intellij/plugin/ANTLRv4SyntaxHighlighter.java
+++ src/main/java/org/antlr/intellij/plugin/ANTLRv4SyntaxHighlighter.java
@@ -19,91 +19,99 @@ import static org.antlr.intellij.plugin.ANTLRv4TokenTypes.*;
 public class ANTLRv4SyntaxHighlighter extends SyntaxHighlighterBase {
     public static final TextAttributesKey KEYWORD =
         createTextAttributesKey("ANTLRv4_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
-
+    
+    public static final TextAttributesKey SPECIAL_KEYWORD =
+        createTextAttributesKey("ANTLRv4_SPECIAL_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
+    
+    
     public static final TextAttributesKey RULENAME =
         createTextAttributesKey("ANTLRv4_RULENAME", DefaultLanguageHighlighterColors.PARAMETER);
-
+    
     public static final TextAttributesKey TOKENNAME =
         createTextAttributesKey("ANTLRv4_TOKENNAME", DefaultLanguageHighlighterColors.INSTANCE_FIELD);
-
+    
     public static final TextAttributesKey STRING =
         createTextAttributesKey("ANTLRv4_STRING", DefaultLanguageHighlighterColors.STRING);
-
+    
     public static final TextAttributesKey LINE_COMMENT =
         createTextAttributesKey("ANTLRv4_LINE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
-
+    
     public static final TextAttributesKey DOC_COMMENT =
         createTextAttributesKey("ANTLRv4_DOC_COMMENT", DefaultLanguageHighlighterColors.DOC_COMMENT);
-
+    
     public static final TextAttributesKey BLOCK_COMMENT =
         createTextAttributesKey("ANTLRv4_BLOCK_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT);
-
+    
     public static final TextAttributesKey INT =
         createTextAttributesKey("ANTLRv4_INT", DefaultLanguageHighlighterColors.NUMBER);
-
+    
     public static final TextAttributesKey BRACE =
         createTextAttributesKey("ANTLRv4_BRACES", DefaultLanguageHighlighterColors.BRACES);
-
-
+    
+    
     private static final TextAttributesKey[] BAD_CHAR_KEYS = pack(HighlighterColors.BAD_CHARACTER);
     private static final TextAttributesKey[] STRING_KEYS = pack(STRING);
     private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{LINE_COMMENT, DOC_COMMENT, BLOCK_COMMENT};
     private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
-
-
+    
+    
     @Override
     public @NotNull Lexer getHighlightingLexer() {
         return new ANTLRv4LexerAdaptor(new ANTLRv4Lexer(null));
     }
-
-
+    
+    
     @Override
     public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
         if (KEYWORDS.contains(tokenType)) {
             return pack(KEYWORD);
         }
-
+        
+        if (ANTLRv4TokenTypes.SPECIAL_KEYWORD.contains(tokenType)) {
+            return pack(SPECIAL_KEYWORD);
+        }
+        
         if (BRACES.contains(tokenType)) {
             return pack(BRACE);
         }
-
+        
         if (Objects.equals(tokenType, TOKEN_ELEMENT_TYPES.get(ANTLRv4Lexer.TOKEN_REF))) {
             return pack(TOKENNAME);
         }
-
+        
         if (Objects.equals(tokenType, TOKEN_ELEMENT_TYPES.get(ANTLRv4Lexer.POUND))) {
-            return pack(RULENAME);
+            return pack(TOKENNAME);
         }
-
+        
         if (Objects.equals(tokenType, TOKEN_ELEMENT_TYPES.get(ANTLRv4Lexer.RULE_REF))) {
             return pack(RULENAME);
         }
-
+        
         if (Objects.equals(tokenType, TOKEN_ELEMENT_TYPES.get(ANTLRv4Lexer.INT))) {
             return pack(INT);
         }
-
+        
         if (Objects.equals(tokenType, TOKEN_ELEMENT_TYPES.get(ANTLRv4Lexer.STRING_LITERAL))
             || Objects.equals(tokenType, TOKEN_ELEMENT_TYPES.get(ANTLRv4Lexer.UNTERMINATED_STRING_LITERAL))) {
             return STRING_KEYS;
         }
-
+        
         if (Objects.equals(tokenType, TOKEN_ELEMENT_TYPES.get(ANTLRv4Lexer.BLOCK_COMMENT))) {
             return COMMENT_KEYS;
         }
-
+        
         if (Objects.equals(tokenType, TOKEN_ELEMENT_TYPES.get(ANTLRv4Lexer.DOC_COMMENT))) {
             return COMMENT_KEYS;
         }
-
+        
         if (Objects.equals(tokenType, TOKEN_ELEMENT_TYPES.get(ANTLRv4Lexer.LINE_COMMENT))) {
             return COMMENT_KEYS;
         }
-
+        
         if (Objects.equals(tokenType, BAD_TOKEN_TYPE)) {
             return BAD_CHAR_KEYS;
         }
-
+        
         return EMPTY_KEYS;
     }
 }
