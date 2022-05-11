97cab1e5717c7a09b466c4b13d747dc217df4ce1
Patrick Lindenberg <heapdump@icloud.com>
2022-05-11 12:38:20 +0200

    update syntax coloring settings page add special keyword extend example text

--- src/main/java/org/antlr/intellij/plugin/configdialogs/ANTLRv4ColorsPage.java
+++ src/main/java/org/antlr/intellij/plugin/configdialogs/ANTLRv4ColorsPage.java
@@ -5,8 +5,8 @@ import com.intellij.openapi.fileTypes.SyntaxHighlighter;
 import com.intellij.openapi.options.colors.AttributesDescriptor;
 import com.intellij.openapi.options.colors.ColorDescriptor;
 import com.intellij.openapi.options.colors.ColorSettingsPage;
-import org.antlr.intellij.plugin.ANTLRv4SyntaxHighlighter;
 import org.antlr.intellij.plugin.ANTLRv4Icons;
+import org.antlr.intellij.plugin.ANTLRv4SyntaxHighlighter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -18,71 +18,73 @@ public class ANTLRv4ColorsPage implements ColorSettingsPage {
         new AttributesDescriptor("Lexer rule", ANTLRv4SyntaxHighlighter.TOKENNAME),
         new AttributesDescriptor("Parser rule", ANTLRv4SyntaxHighlighter.RULENAME),
         new AttributesDescriptor("Keyword", ANTLRv4SyntaxHighlighter.KEYWORD),
+        new AttributesDescriptor("Special keyword", ANTLRv4SyntaxHighlighter.SPECIAL_KEYWORD),
         new AttributesDescriptor("String", ANTLRv4SyntaxHighlighter.STRING),
         new AttributesDescriptor("Number", ANTLRv4SyntaxHighlighter.INT),
         new AttributesDescriptor("Braces", ANTLRv4SyntaxHighlighter.BRACE),
         new AttributesDescriptor("Block comment", ANTLRv4SyntaxHighlighter.BLOCK_COMMENT),
         new AttributesDescriptor("Line comment", ANTLRv4SyntaxHighlighter.LINE_COMMENT),
         new AttributesDescriptor("Doc comment", ANTLRv4SyntaxHighlighter.DOC_COMMENT),
-
+        
     };
-
-
+    
+    
     @Nullable
     @Override
     public Icon getIcon() {
         return ANTLRv4Icons.FILE;
     }
-
-
+    
+    
     @NotNull
     @Override
     public SyntaxHighlighter getHighlighter() {
         return new ANTLRv4SyntaxHighlighter();
     }
-
-
+    
+    
     @NotNull
     @Override
     public String getDemoText() {
         return
-            "grammar Foo;\n\n" + "options { tokenVocab=DSPLLexer; }\n\n" +
-                '\n' +
-                "compilationUnit : STUFF EOF;\n" +
-                '\n' +
-                "STUFF : 'stuff' -> pushMode(OTHER_MODE);\n" +
-                "WS : [ \\t]+ -> channel(HIDDEN);\n" +
-                "NEWLINE : [\\r\\n]+ -> type(WS);\n" +
-                "BAD_CHAR : . -> skip;\n" +
-                "INT : [1..9] [0..9]*\n" +
-                '\n' +
-                "mode OTHER_MODE;\n" +
-                '\n' +
-                "KEYWORD : 'keyword' -> popMode;\n";
+            "// line comment\ngrammar Foo;\n\n" + "options { tokenVocab=FooLexer; }\n\n" +
+            '\n' +
+            "compilationUnit : STUFF EOF;\n" +
+            "\n/* block comment */" +
+            "STUFF : 'stuff' -> pushMode(OTHER_MODE);\n" +
+            "WS : [ \\t]+ -> channel(HIDDEN);\n" +
+            "NEWLINE : [\\r\\n]+ -> type(WS);\n" +
+            "BAD_CHAR : . -> skip;\n\n" +
+            "fragment\n" +
+            "INT : [1..9] [0..9]*\n" +
+            '\n' +
+            "mode OTHER_MODE;\n" +
+            '\n' +
+            "KEYWORD : 'keyword' -> popMode;\n";
     }
-
-
+    
+    
     @Nullable
     @Override
     public Map<String, TextAttributesKey> getAdditionalHighlightingTagToDescriptorMap() {
         return null;
     }
-
-
+    
+    
     @NotNull
     @Override
     public AttributesDescriptor @NotNull [] getAttributeDescriptors() {
         return ATTRIBUTES;
     }
-
-
+    
+    
     @NotNull
     @Override
     public ColorDescriptor @NotNull [] getColorDescriptors() {
         return ColorDescriptor.EMPTY_ARRAY;
     }
-
-
+    
+    
     @NotNull
     @Override
     public String getDisplayName() {
