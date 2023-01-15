8218299a6c8bb9956df1372d432d062a9109e780
Patrick Lindenberg <heapdump@icloud.com>
2023-01-15 17:48:58 +0100

    modernise

--- src/main/java/org/antlr/intellij/plugin/actions/TestRuleAction.java
+++ src/main/java/org/antlr/intellij/plugin/actions/TestRuleAction.java
@@ -1,21 +1,16 @@
 package org.antlr.intellij.plugin.actions;
 
 import com.intellij.icons.AllIcons;
-import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.LogicalPosition;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
 import org.antlr.intellij.plugin.ANTLRv4PluginController;
 import org.antlr.intellij.plugin.psi.ParserRuleRefNode;
 
-import java.awt.*;
-import java.awt.event.InputEvent;
 import java.awt.event.MouseEvent;
 
 public class TestRuleAction extends AnAction implements DumbAware {
@@ -27,11 +22,11 @@ public class TestRuleAction extends AnAction implements DumbAware {
      */
     @Override
     public void update(AnActionEvent e) {
-        Presentation presentation = e.getPresentation();
+        var presentation = e.getPresentation();
         presentation.setText("Test ANTLR Rule"); // default text
         presentation.setIcon(AllIcons.Actions.Execute);
         
-        VirtualFile grammarFile = MyActionUtils.getGrammarFileFromEvent(e);
+        var grammarFile = MyActionUtils.getGrammarFileFromEvent(e);
         if (grammarFile == null) { // we clicked somewhere outside text or non grammar file
             presentation.setEnabled(false);
             presentation.setVisible(false);
@@ -39,20 +34,20 @@ public class TestRuleAction extends AnAction implements DumbAware {
         }
         
         ParserRuleRefNode r = null;
-        InputEvent inputEvent = e.getInputEvent();
+        var inputEvent = e.getInputEvent();
         if (inputEvent instanceof MouseEvent) { // this seems to be after update() called 2x and we have selected the action
             r = MyActionUtils.getParserRuleSurroundingRef(e);
         } else {
             // If editor component, mouse event not happened yet to update caret so must ask for mouse position
-            Editor editor = e.getData(PlatformDataKeys.EDITOR);
+            var editor = e.getData(PlatformDataKeys.EDITOR);
             if (editor != null) {
-                Point mousePosition = editor.getContentComponent().getMousePosition();
+                var mousePosition = editor.getContentComponent().getMousePosition();
                 if (mousePosition != null) {
-                    LogicalPosition pos = editor.xyToLogicalPosition(mousePosition);
-                    int offset = editor.logicalPositionToOffset(pos);
-                    PsiFile file = e.getData(LangDataKeys.PSI_FILE);
+                    var pos = editor.xyToLogicalPosition(mousePosition);
+                    var offset = editor.logicalPositionToOffset(pos);
+                    var file = e.getData(LangDataKeys.PSI_FILE);
                     if (file != null) {
-                        PsiElement el = file.findElementAt(offset);
+                        var el = file.findElementAt(offset);
                         if (el != null) {
                             r = MyActionUtils.getParserRuleSurroundingRef(el);
                         }
@@ -69,7 +64,7 @@ public class TestRuleAction extends AnAction implements DumbAware {
         }
         
         presentation.setVisible(true);
-        String ruleName = r.getText();
+        var ruleName = r.getText();
         if (Character.isLowerCase(ruleName.charAt(0))) {
             presentation.setEnabled(true);
             presentation.setText("Test Rule " + ruleName);
@@ -85,21 +80,21 @@ public class TestRuleAction extends AnAction implements DumbAware {
             LOG.error("actionPerformed no project for " + e);
             return; // whoa!
         }
-        VirtualFile grammarFile = MyActionUtils.getGrammarFileFromEvent(e);
+        var grammarFile = MyActionUtils.getGrammarFileFromEvent(e);
         if (grammarFile == null) return;
         
         LOG.info("actionPerformed " + grammarFile);
         
-        ANTLRv4PluginController controller = ANTLRv4PluginController.getInstance(e.getProject());
+        var controller = ANTLRv4PluginController.getInstance(e.getProject());
         controller.getPreviewWindow().show(null);
         
-        ParserRuleRefNode r = MyActionUtils.getParserRuleSurroundingRef(e);
+        var r = MyActionUtils.getParserRuleSurroundingRef(e);
         if (r == null) {
             return; // weird. no rule name.
         }
-        String ruleName = r.getText();
-        FileDocumentManager docMgr = FileDocumentManager.getInstance();
-        Document doc = docMgr.getDocument(grammarFile);
+        var ruleName = r.getText();
+        var docMgr = FileDocumentManager.getInstance();
+        var doc = docMgr.getDocument(grammarFile);
         if (doc != null) {
             docMgr.saveDocument(doc);
         }
