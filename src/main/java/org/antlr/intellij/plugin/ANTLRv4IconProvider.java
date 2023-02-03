3b649678fe3c67dd246d19f0df1a6a76b64eb30d
Patrick Lindenberg <heapdump@icloud.com>
2023-02-03 12:59:26 +0100

    cleanup language

--- src/main/java/org/antlr/intellij/plugin/ANTLRv4IconProvider.java
+++ src/main/java/org/antlr/intellij/plugin/ANTLRv4IconProvider.java
@@ -17,13 +17,20 @@ public class ANTLRv4IconProvider extends IconProvider {
     public Icon getIcon(@NotNull PsiElement element, int flags) {
         if (element instanceof LexerRuleRefNode) {
             return ANTLRv4Icons.LEXER_RULE;
-        } else if (element instanceof ParserRuleRefNode) {
+        }
+        
+        if (element instanceof ParserRuleRefNode) {
             return ANTLRv4Icons.PARSER_RULE;
-        } else if (element instanceof ANTLRv4FileRoot) {
+        }
+        
+        if (element instanceof ANTLRv4FileRoot) {
             return ANTLRv4Icons.FILE;
-        } else if (element instanceof ModeSpecNode) {
+        }
+        
+        if (element instanceof ModeSpecNode) {
             return ANTLRv4Icons.MODE;
         }
+        
         return null;
     }
 }
