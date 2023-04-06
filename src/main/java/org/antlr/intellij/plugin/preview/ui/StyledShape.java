1d17b6c53a04174fcc316288353f82a33afb4f58
Patrick Lindenberg <heapdump@icloud.com>
2023-04-06 21:51:52 +0200

    remove unused methods due to additional style attributes

--- src/main/java/org/antlr/intellij/plugin/preview/ui/StyledShape.java
+++ src/main/java/org/antlr/intellij/plugin/preview/ui/StyledShape.java
@@ -1,19 +1,12 @@
 package org.antlr.intellij.plugin.preview.ui;
 
 
-import com.intellij.ui.JBColor;
-
 import java.awt.geom.Rectangle2D;
 
 /**
  * Abstract shape with styled elements.
  */
 public abstract class StyledShape extends StyledElement {
-    // separate outline color
-    private JBColor outlineColor;
-    private boolean filled = true;
-    
-    
     /**
      * Empty constructor (properties may inherit by getter/setter).
      */
@@ -31,30 +24,5 @@ public abstract class StyledShape extends StyledElement {
      */
     public StyledShape(StyledElement parent, Rectangle2D viewport, StyleProperties styles) {
         super(parent, viewport, styles);
-    }
-    
-    
-    public boolean hasOutlineColor() {
-        return outlineColor != null;
-    }
-    
-    
-    public JBColor getOutlineColor() {
-        return outlineColor;
-    }
-    
-    
-    public void setOutlineColor(JBColor outlineColor) {
-        this.outlineColor = outlineColor;
-    }
-    
-    
-    public boolean isFilled() {
-        return filled;
-    }
-    
-    
-    public void setFilled(boolean filled) {
-        this.filled = filled;
     }
 }
