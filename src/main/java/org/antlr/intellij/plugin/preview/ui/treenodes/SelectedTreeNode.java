33dfd507bd1fddc375eb119cba8a96ed4215353f
Patrick Lindenberg <heapdump@icloud.com>
2023-04-06 22:01:25 +0200

    update

--- src/main/java/org/antlr/intellij/plugin/preview/ui/treenodes/SelectedTreeNode.java
+++ src/main/java/org/antlr/intellij/plugin/preview/ui/treenodes/SelectedTreeNode.java
@@ -1,6 +1,5 @@
 package org.antlr.intellij.plugin.preview.ui.treenodes;
 
-import com.intellij.ui.JBColor;
 import org.antlr.intellij.plugin.preview.ui.DefaultStyles;
 import org.antlr.intellij.plugin.preview.ui.StyleProperties;
 import org.antlr.intellij.plugin.preview.ui.StyledElement;
@@ -12,13 +11,8 @@ import java.awt.geom.Rectangle2D;
  */
 public class SelectedTreeNode extends BasicStyledTreeNode {
     
-    
     public SelectedTreeNode(StyledElement parent, Rectangle2D viewport, StyleProperties base, boolean compact) {
         super(parent, viewport, DefaultStyles.SELECTED_NODE_STYLE, true, compact);
         this.setStyleProperties(base);
-        
-        styleProperties.setStroke(DefaultStyles.THICK_STROKE);
-        setOutlineColor((JBColor) (shape.hasOutlineColor() ? getOutlineColor() : getForeground()).brighter());
-        setBackground((JBColor) getBackground().brighter());
     }
 }
