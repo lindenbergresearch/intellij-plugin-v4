ade73a971fe21cf005603447e87be68749892df8
Patrick Lindenberg <heapdump@icloud.com>
2025-02-16 14:14:19 +0100

    reformat live templates and adapt to new plugin spec

--- src/main/java/org/antlr/intellij/plugin/templates/ANTLRLiveTemplateContext.java
+++ src/main/java/org/antlr/intellij/plugin/templates/ANTLRLiveTemplateContext.java
@@ -1,5 +1,6 @@
 package org.antlr.intellij.plugin.templates;
 
+import com.intellij.codeInsight.template.TemplateActionContext;
 import com.intellij.codeInsight.template.TemplateContextType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -7,36 +8,50 @@ import com.intellij.psi.util.PsiUtilBase;
 import org.antlr.intellij.plugin.ANTLRv4Language;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public abstract class ANTLRLiveTemplateContext extends TemplateContextType {
-    public ANTLRLiveTemplateContext(
-        @NotNull @NonNls String id,
-        @NotNull String presentableName,
-        @Nullable Class<? extends TemplateContextType> baseContextType
-    ) {
-        super(id, presentableName, baseContextType);
+abstract class ANTLRLiveTemplateContext extends TemplateContextType {
+    public ANTLRLiveTemplateContext(@NotNull @NonNls String id) {
+        super(id);
+    }
+    
+    
+    @Override
+    public boolean isInContext(@NotNull TemplateActionContext templateActionContext) {
+        var file = templateActionContext.getFile();
+        var offset = templateActionContext.getEndOffset();
+        
+        if (!PsiUtilBase.getLanguageAtOffset(file, offset).isKindOf(ANTLRv4Language.INSTANCE)) {
+            return false;
+        }
+        
+        if (offset == file.getTextLength()) { // allow at EOF
+            offset--;
+        }
+        
+        var element = file.findElementAt(offset);
+        
+        return element != null && isInContext(file, element, offset);
     }
     
     
     protected abstract boolean isInContext(@NotNull PsiFile file, @NotNull PsiElement element, int offset);
-    
-    
-    @Override
-    public boolean isInContext(@NotNull PsiFile file, int offset) {
-        // offset is where cursor or insertion point is I guess
-        if (!PsiUtilBase.getLanguageAtOffset(file, offset).isKindOf(ANTLRv4Language.INSTANCE)) {
-            return false;
-        }
-        if (offset == file.getTextLength()) { // allow at EOF
-            offset--;
-        }
-        PsiElement element = file.findElementAt(offset);
-        
-        if (element == null) {
-            return false;
-        }
-        
-        return isInContext(file, element, offset);
-    }
+//
+//
+//    @Override
+//    public boolean isInContext(@NotNull PsiFile file, int offset) {
+//        // offset is where cursor or insertion point is I guess
+//        if (!PsiUtilBase.getLanguageAtOffset(file, offset).isKindOf(ANTLRv4Language.INSTANCE)) {
+//            return false;
+//        }
+//        if (offset == file.getTextLength()) { // allow at EOF
+//            offset--;
+//        }
+//        PsiElement element = file.findElementAt(offset);
+//
+//        if (element == null) {
+//            return false;
+//        }
+//
+//        return isInContext(file, element, offset);
+//    }
 }
--- src/main/java/org/antlr/intellij/plugin/templates/OutsideRuleContext.java
+++ src/main/java/org/antlr/intellij/plugin/templates/OutsideRuleContext.java
@@ -4,41 +4,42 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import org.antlr.intellij.plugin.parser.ANTLRv4Parser;
 import org.antlr.intellij.plugin.parsing.ParsingUtils;
-import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 import org.jetbrains.annotations.NotNull;
 
 public class OutsideRuleContext extends ANTLRLiveTemplateContext {
     public OutsideRuleContext() {
-        super("ANTLR_OUTSIDE", "Outside rule", ANTLRGenericContext.class);
+        super("ANTLR_OUTSIDE");
     }
     
     
     @Override
-    public boolean isInContext(@NotNull PsiFile file, PsiElement element, int offset) {
-        CommonTokenStream tokens = ParsingUtils.tokenizeANTLRGrammar(file.getText());
-        Token tokenUnderCursor = ParsingUtils.getTokenUnderCursor(tokens, offset);
+    public boolean isInContext(@NotNull PsiFile file, @NotNull PsiElement element, int offset) {
+        var tokens = ParsingUtils.tokenizeANTLRGrammar(file.getText());
+        var tokenUnderCursor = ParsingUtils.getTokenUnderCursor(tokens, offset);
         if (tokenUnderCursor == null) {
             return false; // sometimes happens at the eof
         }
-        int tokenIndex = tokenUnderCursor.getTokenIndex();
-        Token nextRealToken = ParsingUtils.nextRealToken(tokens, tokenIndex);
-        Token previousRealToken = ParsingUtils.previousRealToken(tokens, tokenIndex);
+        
+        var tokenIndex = tokenUnderCursor.getTokenIndex();
+        var nextRealToken = ParsingUtils.nextRealToken(tokens, tokenIndex);
+        var previousRealToken = ParsingUtils.previousRealToken(tokens, tokenIndex);
         
         if (nextRealToken == null || previousRealToken == null) {
             return false;
         }
         
-        int previousRealTokenType = previousRealToken.getType();
-        int nextRealTokenType = nextRealToken.getType();
+        var previousRealTokenType = previousRealToken.getType();
+        var nextRealTokenType = nextRealToken.getType();
         
         if (previousRealTokenType == ANTLRv4Parser.BEGIN_ACTION) {
             // make sure we're not in a rule; has to be @lexer::header {...} stuff
-            Token prevPrevRealToken = ParsingUtils.previousRealToken(tokens, previousRealToken.getTokenIndex());
+            var prevPrevRealToken = ParsingUtils.previousRealToken(tokens, previousRealToken.getTokenIndex());
             if (prevPrevRealToken == null) {
                 return false;
             }
-            Token prevPrevPrevRealToken = ParsingUtils.previousRealToken(tokens, prevPrevRealToken.getTokenIndex());
+            
+            var prevPrevPrevRealToken = ParsingUtils.previousRealToken(tokens, prevPrevRealToken.getTokenIndex());
             if (prevPrevPrevRealToken == null) {
                 return false;
             }
@@ -48,11 +49,11 @@ public class OutsideRuleContext extends ANTLRLiveTemplateContext {
             }
         }
         
-        boolean okBefore =
+        var okBefore =
             previousRealTokenType == ANTLRv4Parser.RBRACE ||
                 previousRealTokenType == ANTLRv4Parser.SEMI ||
                 previousRealTokenType == ANTLRv4Parser.BEGIN_ACTION;
-        boolean okAfter =
+        var okAfter =
             nextRealTokenType == ANTLRv4Parser.TOKEN_REF ||
                 nextRealTokenType == ANTLRv4Parser.RULE_REF ||
                 nextRealTokenType == Token.EOF;
